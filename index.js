93a304dbe349ed359ab279e9ba405e02b44e1e86
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2022-01-19 21:46:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const memoFibonacci = (number, memo = {}) => {
+  if (memo[number]) return memo[number];
+  if (number <= 1) return 1;
+  return (memo[number] =
+    memoFibonacci(number - 1, memo) + memoFibonacci(number - 2, memo));
+};
+
+console.log(memoFibonacci(0));
+console.log(memoFibonacci(1));
+console.log(memoFibonacci(20));
+console.log(memoFibonacci(40));
+console.log(memoFibonacci(50));
+console.log(memoFibonacci(80));
+console.log(memoFibonacci(90));
+
+const oldwayFibonacci = (number) => {
+  if (number <= 1) return 1;
+  return oldwayFibonacci(number - 1) + oldwayFibonacci(number - 2);
+};
+
+console.log(oldwayFibonacci(0));
+console.log(oldwayFibonacci(1));
+console.log(oldwayFibonacci(20));
+console.log(oldwayFibonacci(40));
+console.log(oldwayFibonacci(42));
+console.log(oldwayFibonacci(43));
+console.log(oldwayFibonacci(44));
+console.log(oldwayFibonacci(45));
